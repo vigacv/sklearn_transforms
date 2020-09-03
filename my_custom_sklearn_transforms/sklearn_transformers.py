887f97981cacc4baf766be6c153b49f80efad6ab
Gabriel Carmelo <65205479+vigacv@users.noreply.github.com>
2020-09-02 21:41:02 -0500

    Dividido en clases
    Para hacer mas comprensible el pipeline y mejorar debugging

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -8,9 +8,7 @@ import pandas as pd
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
-        self.scaler = RobustScaler()
         self.columns = columns
-        self.si = SimpleImputer(missing_values=np.nan,strategy='mean', fill_value=0, verbose=0, copy=True)
 
     def fit(self, X, y=None):
         return self
@@ -19,10 +17,28 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Primero copiamos el dataframe de datos de entrada 'X'
         data = X.copy()
         # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
-        data_dp = data.drop(labels=self.columns, axis='columns')
-        self.scaler.fit(data_dp[data_dp.columns[:12]])
-        self.si.fit(data_dp[data_dp.columns[:12]])
-        data_si = pd.DataFrame.from_records(data=self.si.transform(X=data_dp[data_dp.columns[:12]]),columns=data_dp.columns[:12])
-        data_sc = pd.DataFrame.from_records(data=self.scaler.transform(X=data_si[data_si.columns[:12]]),columns=data_si.columns[:12])
-        data_sc = data_sc.join(data_dp['PROFILE'])
-        return data_sc
+        return data.drop(labels=self.columns, axis='columns')
+
+class Imputation():
+    def __init__(self):
+        self.si = SimpleImputer(missing_values=np.nan,strategy='mean', fill_value=0, verbose=0, copy=True)
+    def fit(self, X):
+        data = X.copy()
+        self.si.fit(data[data.columns[:12]])
+        return self
+    def transform(self, X):
+        data = X.copy()
+        data_si = pd.DataFrame.from_records(data=self.si.transform(X=data[data.columns[:12]]),columns=data.columns[:12])
+        return data_si.join(data['PROFILE'])
+    
+class Normalization():
+    def __init__(self):
+        self.scaler = RobustScaler()
+    def fit(self, X):
+        data = X.copy()
+        self.scaler.fit(data[data.columns[:12]])
+        return self
+    def transform(self, X):
+        data = X.copy()
+        data_sc = pd.DataFrame.from_records(data=self.scaler.transform(X=data[data.columns[:12]]),columns=data.columns[:12])
+        return data_sc.join(data['PROFILE'])
