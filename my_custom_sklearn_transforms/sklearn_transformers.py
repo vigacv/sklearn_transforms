1280f75cd8f4aa7155914526dd9ad099b834abc2
Gabriel Carmelo <65205479+vigacv@users.noreply.github.com>
2020-09-02 20:56:08 -0500

    Agregada importacion de SI

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import RobustScaler
+from sklearn.impute import SimpleImputer
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
