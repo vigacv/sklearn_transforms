dad01437b76de32c7c553c32d5060618872a2fd0
Gabriel Carmelo <65205479+vigacv@users.noreply.github.com>
2020-09-02 20:50:29 -0500

    Agregadas transformaciones personalizadas
    Para el desafio 1 del maraton BTC de IBM 2020

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,16 +1,25 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import RobustScaler
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
+        self.scaler = RobustScaler()
         self.columns = columns
+        self.si = SimpleImputer(missing_values=np.nan,strategy='mean', fill_value=0, verbose=0, copy=True)
 
     def fit(self, X, y=None):
         return self
-
+    
     def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        # Primero copiamos el dataframe de datos de entrada 'X'
         data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+        # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
+        data_dp = data.drop(labels=self.columns, axis='columns')
+        self.scaler.fit(data_dp[data_dp.columns[:12]])
+        self.si.fit(data_dp[data_dp.columns[:12]])
+        data_si = pd.DataFrame.from_records(data=self.si.transform(X=data_dp[data_dp.columns[:12]]),columns=data_dp.columns[:12])
+        data_sc = pd.DataFrame.from_records(data=self.scaler.transform(X=data_si[data_si.columns[:12]]),columns=data_si.columns[:12])
+        data_sc = data_sc.join(data_dp['PROFILE'])
+        return data_sc
